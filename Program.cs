91bf2c665f82bb334d35c78d3e2efa3652536212
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-12-05 21:14:07 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace garage_of_gary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
